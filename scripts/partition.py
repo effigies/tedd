d9fe2001c243ca1c62356ccf813c348dda47123c
Chris Johnson <effigies@gmail.com>
2012-06-05 18:35:50 -0400

    Move mount/unmount into partition

--- scripts/partition.py
+++ scripts/partition.py
@@ -132,3 +132,31 @@ def verifyLinux(part,cwd=os.getcwd()):
                 return True
     unmount_partition(path, flag)
     return False
+
+def mount_partition(part):
+    f = open('/proc/mounts')
+    mounts = [mount.split() for mount in f if mount]
+    f.close()
+
+    existing = [mount for mount in mounts if mount[0] == part.path]
+    if existing:
+        return existing[0][1], False
+
+    mountpoint = mkdtemp()
+    if os.system("mount -t %s -o ro %s %s" %
+                (part.fileSystem.type, part.path, mountpoint)
+                ) == 0:
+        return mountpoint, True
+    return None
+
+def unmount_partition(mountpoint, flag):
+    if flag:
+        time.sleep(1)
+        if not os.system("umount %s" % mountpoint):
+            os.rmdir(mountpoint)
+        else:
+            time.sleep(2)
+            if not os.system("umount %s" % mountpoint):
+                os.rmdir(mountpoint)
+            else:
+                debugPrint("WARNING: Could not unmount %s" % mountpoint)
